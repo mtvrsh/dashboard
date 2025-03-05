c1a97865c9c790965aeb00af63405996b9711602
mtvrs <mtvrs@tuta.io>
2025-03-05 11:25:07 +0100

    Remove javascript

--- api/all.go
+++ api/all.go
@@ -1,5 +1,13 @@
 package api
 
+import (
+	"fmt"
+	"html/template"
+	"math"
+	"strconv"
+	"strings"
+)
+
 type All struct {
 	Hostname   string
 	Uptime     string
@@ -7,11 +15,23 @@ type All struct {
 	DisksUsage map[string]DiskUsage
 	// DirsInUse map[string]bool
 	// Temperatures map[string]int
+
+	CommandStdout string
 }
 
 type DiskUsage struct {
-	Total       string
-	Used        string
-	UsedPercent string
-	Free        string
+	Size       string
+	Used       string
+	UsePercent string
+	Avail      string
+}
+
+func (du DiskUsage) UsePercentStyle() template.CSS {
+	p, err := strconv.ParseFloat(strings.TrimSuffix(du.UsePercent, "%"), 64)
+	if err != nil {
+		return ""
+	}
+	red := min(255, int(math.Floor((p/100)*255)))
+	green := 255 - red
+	return template.CSS(fmt.Sprintf("color: rgb(%d, %d, 0)", red, green))
 }
