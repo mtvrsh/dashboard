4519cd552fcf96a082cf9428fa369639b6e8bc2f
mtvrs <mtvrs@tuta.io>
2025-03-05 11:25:07 +0100

    Remove javascript

--- server.go
+++ server.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"embed"
-	"encoding/json"
+	_ "embed"
 	"fmt"
-	"io/fs"
+	"html/template"
 	"log"
 	"net"
 	"net/http"
@@ -15,11 +14,12 @@ import (
 
 const defaultPort = 8080
 
-//go:embed static/*
-var content embed.FS
+//go:embed style.css
+var css []byte
 
 type server struct {
 	config config
+	main   *template.Template
 }
 
 func newServer() server {
@@ -27,16 +27,15 @@ func newServer() server {
 }
 
 func (s *server) serve() error {
-	http.Handle("GET /", staticHandler())
-	http.HandleFunc("GET /commands", s.commandsHandler)
-	http.HandleFunc("PUT /command/{command}", s.commandHandler)
-	http.HandleFunc("GET /all", s.allHandler)
+	s.main = template.Must(template.New("index.template").ParseFiles("index.template"))
+	http.HandleFunc("GET /", s.mainHandler)
+	http.HandleFunc("GET /style.css", s.styleHandler)
 
 	addr := net.JoinHostPort(s.config.Address, fmt.Sprint(s.config.Port))
 	return http.ListenAndServe(addr, nil)
 }
 
-func (s *server) allHandler(w http.ResponseWriter, r *http.Request) {
+func (s *server) mainHandler(w http.ResponseWriter, r *http.Request) {
 	all, err := getSystemInfo(s.config.WatchMountpoints)
 	if err != nil {
 		log.Printf("collection failed %v", err)
@@ -44,30 +43,20 @@ func (s *server) allHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	all.Commands = s.getCommands()
+	all.CommandStdout = s.execCommand(r.FormValue("command"))
 
-	data, err := json.Marshal(all)
+	err = s.main.Execute(w, all)
 	if err != nil {
-		log.Printf("json encoding failed: %v", err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	_, err = w.Write(data)
-	if err != nil {
-		log.Printf("failed to write response: %v", err)
+		log.Print(err)
 	}
 }
 
-func (s *server) commandsHandler(w http.ResponseWriter, r *http.Request) {
-	commands := s.getCommands()
-	data, err := json.Marshal(commands)
+func (s *server) styleHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Cache-Control", "immutable, max-age=86400")
+	w.Header().Set("Content-Type", "text/css")
+	_, err := w.Write(css)
 	if err != nil {
-		log.Printf("json encoding failed: %v", err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	_, err = w.Write(data)
-	if err != nil {
-		log.Printf("failed to write response: %v", err)
+		log.Print(err)
 	}
 }
 
@@ -80,39 +69,28 @@ func (s *server) getCommands() []string {
 	return commands
 }
 
-func (s *server) commandHandler(w http.ResponseWriter, r *http.Request) {
+func (s *server) execCommand(command string) string {
 	const defaultSize = 256
-	cmdFromPath := s.config.Commands[r.PathValue("command")]
+	if command == "" {
+		return ""
+	}
+	cmdFromPath := s.config.Commands[command]
 	if len(cmdFromPath) == 0 {
-		http.Error(w, "Command does not exist", http.StatusInternalServerError)
-		return
+		log.Printf("unknown command: %q", command)
+		return ""
 	}
 
-	command := exec.Command(cmdFromPath[0])
+	cmd := exec.Command(cmdFromPath[0])
 	if len(cmdFromPath) > 1 {
-		command = exec.Command(cmdFromPath[0], cmdFromPath[1:]...)
+		cmd = exec.Command(cmdFromPath[0], cmdFromPath[1:]...)
 	}
 
-	rawOutput, err := command.CombinedOutput()
-	output := fmt.Sprintf("$ %v\n%s", strings.Join(command.Args, " "),
-		truncate(string(rawOutput), defaultSize, "..."))
+	output, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Printf("command %q failed: %s: %s", command, err, rawOutput)
-		http.Error(w, strings.TrimSpace(output), http.StatusInternalServerError)
-		return
+		log.Printf("command %q failed: %s: %s", command, err, output)
 	}
-	_, err = fmt.Fprint(w, output)
-	if err != nil {
-		log.Printf("failed to write response: %v", err)
-	}
-}
-
-func staticHandler() http.Handler {
-	files, err := fs.Sub(content, "static")
-	if err != nil {
-		panic(err)
-	}
-	return http.FileServerFS(files)
+	return fmt.Sprintf("$ %v\n%s", strings.Join(cmd.Args, " "),
+		truncate(string(output), defaultSize, "..."))
 }
 
 func truncate(s string, max int, ellip string) string {
